db8871691bb06e017e8c76def33bb3e14d9aaa7b
vinesmatthew <mvines1@stu.jsu.edu>
2019-02-09 16:15:03 -0600

    Completed "csvToJson"

--- src/edu/jsu/mcis/Converter.java
+++ src/edu/jsu/mcis/Converter.java
@@ -10,7 +10,7 @@ public class Converter {
     
     /*
     
-        Consider the following CSV data:
+        Consider the following CSV info:
         
         "ID","Total","Assignment 1","Assignment 2","Exam 1"
         "111278","611","146","128","337"
@@ -22,16 +22,16 @@ public class Converter {
         "111276","579","130","111","338"
         "111241","973","236","237","500"
         
-        The corresponding JSON data would be similar to the following (tabs and
+        The corresponding JSON info would be similar to the following (tabs and
         other whitespace have been added for clarity).  Note the curly braces,
         square brackets, and double-quotes!  These indicate which values should
         be encoded as strings, and which values should be encoded as integers!
         
         {
-            "colHeaders":["ID","Total","Assignment 1","Assignment 2","Exam 1"],
-            "rowHeaders":["111278","111352","111373","111305","111399","111160",
+            "col":["ID","Total","Assignment 1","Assignment 2","Exam 1"],
+            "row":["111278","111352","111373","111305","111399","111160",
             "111276","111241"],
-            "data":[[611,146,128,337],
+            "info":[[611,146,128,337],
                     [867,227,228,412],
                     [461,96,90,275],
                     [835,220,217,398],
@@ -43,21 +43,21 @@ public class Converter {
         }
     
         Your task for this program is to complete the two conversion methods in
-        this class, "csvToJson()" and "jsonToCsv()", so that the CSV data shown
+        this class, "csvToJson()" and "jsonToCsv()", so that the CSV info shown
         above can be converted to JSON format, and vice-versa.  Both methods
-        should return the converted data as strings, but the strings do not need
+        should return the converted info as strings, but the strings do not need
         to include the newlines and whitespace shown in the examples; again,
         this whitespace has been added only for clarity.
     
         NOTE: YOU SHOULD NOT WRITE ANY CODE WHICH MANUALLY COMPOSES THE OUTPUT
-        STRINGS!!!  Leave ALL string conversion to the two data conversion
+        STRINGS!!!  Leave ALL string conversion to the two info conversion
         libraries we have discussed, OpenCSV and json-simple.  See the "Data
         Exchange" lecture notes for more details, including example code.
     
     */
     
     @SuppressWarnings("unchecked")
-    public static String csvToJson(String csvString) {
+    public static String csvToJson(String csvString) throws ParseException {
         
         String results = "";
         
@@ -66,11 +66,64 @@ public class Converter {
             CSVReader reader = new CSVReader(new StringReader(csvString));
             List<String[]> full = reader.readAll();
             Iterator<String[]> iterator = full.iterator();
+            String[] colHeaders = iterator.next();
             
-            // INSERT YOUR CODE HERE
+            JSONArray modifyRecord = new JSONArray();
+            LinkedHashMap<String, JSONArray> jsonData = new LinkedHashMap<>();
             
-        }        
-        catch(Exception e) { return e.toString(); }
+            String[] record;
+            
+            int[] rowHeaders = new int[full.size() - 1];
+            String[][] data = new String[full.size() - 1][colHeaders.length - 1];
+            int counter = 0;
+            int counter2 = 0;
+
+            while (iterator.hasNext()) {
+                record = iterator.next();
+                for (int i = 0; i < colHeaders.length; i++) {
+                    if (i == 0) {
+                        rowHeaders[counter] = Integer.parseInt(record[i]);
+                        counter++;
+                    } else {
+                        data[counter2][i - 1] = record[i];
+                    }
+                }
+                counter2++;
+            }
+            
+            counter = 0;
+            for (int i : rowHeaders) {
+                modifyRecord.add(counter, i);
+                counter++;
+            }
+            
+            jsonData.put("rowHeaders", modifyRecord);
+            
+            modifyRecord = new JSONArray();
+            JSONArray modifyRecord2 = new JSONArray();
+            
+            for (int i = 0; i < data.length; i++) {
+                for (int j = 0; j < data[i].length; j++) {
+                    modifyRecord2.add(j, data[i][j]);
+                }
+                modifyRecord.add(i, modifyRecord2);
+                modifyRecord2 = new JSONArray();
+            }
+            
+            jsonData.put("data", modifyRecord);
+            
+            modifyRecord = new JSONArray();
+            counter = 0;
+            
+            for (String i : colHeaders) {
+                modifyRecord.add(counter, i);
+                counter++;
+            }
+            
+            jsonData.put("colHeaders", modifyRecord);
+            
+            results = JSONValue.toJSONString(jsonData);
+        } catch(IOException e) { return e.toString(); }
         
         return results.trim();
         
